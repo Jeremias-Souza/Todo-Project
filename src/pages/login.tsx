c06a678c599911ad10bb62287e47c521fc485d4d
Jeremias-Souza <105684728+Jeremias-Souza@users.noreply.github.com>
2023-10-17 16:25:36 -0300

    ajustes de arquivos

---
+++ src/pages/login.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, provider } from "@/server/firebase.config";
+import { signInWithRedirect } from "firebase/auth";
+import { Button } from "@/components/ui/button";
+import "src/app/globals.css";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+
+export type ItemFormDialogProps = {
+  show: boolean;
+};
+
+export default function Login() {
+  const [user, loading] = useAuthState(auth);
+
+  const signInWithGoogle = () => {
+    signInWithRedirect(auth, provider);
+  };
+
+  const screenPersonal = () => {
+    window.location.href = "/";
+  };
+
+  function getInitials(displayName: string | null): React.ReactNode {
+    throw new Error("Function not implemented.");
+  }
+
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen">
+        <div className="p-4 rounded-md">
+          {loading ? (
+            <div className="loading">
+              <div className="spinner"></div>
+            </div>
+          ) : user ? (
+            <div>
+              <Button onClick={screenPersonal} className="h-15">
+                <Avatar className="right-2">
+                  {user.photoURL ? (
+                    <AvatarImage src={user.photoURL}></AvatarImage>
+                  ) : (
+                    <div className="initials">
+                      {getInitials(user.displayName)}
+                    </div>
+                  )}
+                </Avatar>
+                Entrar com: {user.displayName}
+              </Button>
+            </div>
+          ) : loading ? (
+            <div className="spinner"></div>
+          ) : (
+            <Button onClick={signInWithGoogle}>Fazer login com Google</Button>
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
